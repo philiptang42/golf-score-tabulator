3430f64c8bf78b4ea66d25ac7c39f19cbb8d5537
philiptang42 <philiptang42@gmail.com>
2015-11-28 20:37:58 -0500

    solution

--- golf_score.rb
+++ golf_score.rb
@@ -21,3 +21,23 @@ game_results = [
 
 full_score = 0
 full_par = 0
+
+game_results.each do |hole|
+  full_score += hole[:score]
+  full_par += hole[:par]
+end
+
+puts "Total Strokes: #{full_score}"
+puts "Total Par: #{full_par}"
+
+par_value = full_score - full_par
+
+par_status = ""
+unless par_value >= 0
+  par_status = "under"
+  par_value = par_value * -1
+else
+  par_status = "over"
+end
+
+puts "You were #{par_value} #{par_status} par."
